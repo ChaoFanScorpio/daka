c500b9d6770a96b6c8b17602a72481ee24bbf3ed
张得丑 <18511141234@163.com>
2022-08-22 17:54:53 +0800

    Initial Commit

---
+++ ClockIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClockIn
+//
+//  Created by 张得丑 on 2022/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
